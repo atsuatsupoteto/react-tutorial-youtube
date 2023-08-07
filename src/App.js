7b382150f41293f2d9a11089d97b095d723311cc
atsuatsupoteto <atsuatsu.poteto@gmail.com>
2023-08-08 07:53:14 +0900

    TodoListコンポーネントの作成

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import './App.css';
+import { useState } from "react";
+import TodoList from "./TodoList";
 
 function App() {
+const [todos, setTodos] = useState([
+  { id: 1, name: "Todo1" , completed: false },
+]);
+
   return (
-    <div className="App">Hello React</div>
+    <>
+      <TodoList todos={todos} />
+        <input type="text" />
+        <button>タスクを追加</button>
+        <button>完了したタスクの削除</button>
+        <div>残りのタスク:0</div>
+    </>
   );
 }
 
